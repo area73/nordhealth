097cdb42bb4de463ea69021bb49bf70c27ec9e1a
rerades <rerades@siete3.com>
2025-04-02 08:29:43 +0200

    wip

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterLink } from 'vue-router'
+
+onMounted(() => {
+  const form = document.querySelector('#signup') as HTMLFormElement
+  form?.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const data = new FormData(form)
+    console.log(JSON.stringify(Object.fromEntries(data)))
+  })
+})
 </script>
 <template>
   <div class="signup">
     <h1>This is The sign up page</h1>
+    <form id="signup" action="/success">
+      <provet-stack>
+        <provet-input
+          label="email"
+          name="email"
+          type="email"
+          placeholder="business email"
+          required="true"
+        >
+          <div slot="error">error</div>
+        </provet-input>
+        <provet-input
+          label="password"
+          name="password"
+          type="password"
+          placeholder="minimum 8 characters"
+          required="true"
+          error="This field is required"
+        >
+        </provet-input>
+        <provet-button variant="primary" type="submit" error="This field is required"
+          >Submit</provet-button
+        >
+      </provet-stack>
+    </form>
+
     <RouterLink to="/success">success</RouterLink>
   </div>
 </template>
